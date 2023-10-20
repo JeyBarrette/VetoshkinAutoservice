118da33bb6babdfc06372446c08cf22d7437e138
JeyBarrette <olegpetrow2001@gmail.com>
2023-10-20 16:39:14 +0500

    lab8 (12 пунктов)

--- VetoshkinAutoservice/VetoshkinAutoservice/VetoshkinModel.Context.cs
+++ VetoshkinAutoservice/VetoshkinAutoservice/VetoshkinModel.Context.cs
@@ -45,6 +45,7 @@ namespace VetoshkinAutoservice
         public virtual DbSet<ProductSale> ProductSale { get; set; }
         public virtual DbSet<Service> Service { get; set; }
         public virtual DbSet<ServicePhoto> ServicePhoto { get; set; }
+        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Tag> Tag { get; set; }
     }
 }
